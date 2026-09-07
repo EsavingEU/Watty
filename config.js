b330dc30430fc28a0660dd2c7cdce9967c410d1a
EsavingEU <marco.dematte89@gmail.com>
2026-09-07 15:20:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// This file contains sensitive configuration data
+// DO NOT commit this file to version control
+// Add this file to .gitignore
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsX-02rmrO4SRmoTiksTtbCREoBxfFgBI",
+    authDomain: "wattyesaving.firebaseapp.com",
+    projectId: "wattyesaving",
+    storageBucket: "wattyesaving.firebasestorage.app",
+    messagingSenderId: "101400604075",
+    appId: "1:101400604075:web:6d1434a8866ff48fb376ee"
+};
